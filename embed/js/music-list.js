8a31f89ee3c2c71ab50595870d23b540b3862b1d
abhishekpoojaryy <120596735+abhishekpoojaryy@users.noreply.github.com>
2023-07-26 11:40:30 +0530

    Add files via upload

---
+++ embed/js/music-list.js
@@ -0,0 +1,54 @@
+let allMusic = [
+    {
+      name: "Lofi Study Music 1",
+      artist: "Unknown",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Lofi Study Music 2",
+      artist: "Unknown",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Lofi Study Music 3",
+      artist: "Unknown",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Lofi Study Music 4",
+      artist: "Unknown",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Lofi Study Music 5",
+      artist: "Unknown",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Lofi Study Music 6",
+      artist: "Unknown",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Lofi Study Music 7",
+      artist: "Unknown",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Lofi Study Music 8",
+      artist: "Unknown",
+      img: "music-8",
+      src: "music-8"
+    },
+    
+  
+  
+    
+  ];
